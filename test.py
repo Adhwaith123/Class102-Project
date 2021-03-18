620094b4e65e38a6706621d0ddc7b8d74572425a
Adhwaith123 <62431227+Adhwaith123@users.noreply.github.com>
2021-03-18 18:46:05 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+import cv2;
+
+def takesnapshot():
+    videoCaptureObject=cv2.VideoCapture(0)
+    result=True
+    
+    while(result):
+        ret,frame=videoCaptureObject.read()
+        cv2.imwrite("picture.jpg",frame)
+        result=False
+
+    videoCaptureObject.release()
+    cv2.destroyAllWindows()    
+
+takesnapshot()
